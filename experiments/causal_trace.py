c95d681e298cb86c96da16770cc407f222123e3c
Reza Madani <rezamadani.ai@gmail.com>
2024-07-16 20:02:12 +0200

    Remove font for plots

--- experiments/causal_trace.py
+++ experiments/causal_trace.py
@@ -586,44 +586,43 @@ def plot_trace_heatmap(result, savepdf=None, title=None, xlabel=None, modelname=
     for i in range(*result["subject_range"]):
         labels[i] = labels[i] + "*"
 
-    with plt.rc_context(rc={"font.family": "Times New Roman"}):
-        fig, ax = plt.subplots(figsize=(3.5, 2), dpi=200)
-        h = ax.pcolor(
-            differences,
-            cmap={None: "Purples", "None": "Purples", "mlp": "Greens", "attn": "Reds"}[
-                kind
-            ],
-            vmin=low_score #Setting the minimum value of the color bar to 0
-            # vmax=1    # Setting the maximum value of the color bar to 1
-        )
-        ax.invert_yaxis()
-        ax.set_yticks([0.5 + i for i in range(len(differences))])
-        ax.set_xticks([0.5 + i for i in range(0, differences.shape[1] - 6, 5)])
-        ax.set_xticklabels(list(range(0, differences.shape[1] - 6, 5)))
-        ax.set_yticklabels(labels)
-        if not modelname:
-            modelname = "GPT"
-        if not kind:
-            ax.set_title("Impact of restoring state after corrupted input")
-            ax.set_xlabel(f"single restored layer within {modelname}")
-        else:
-            kindname = "MLP" if kind == "mlp" else "Attn"
-            ax.set_title(f"Impact of restoring {kindname} after corrupted input")
-            ax.set_xlabel(f"center of interval of {window} restored {kindname} layers")
-        cb = plt.colorbar(h)
-        if title is not None:
-            ax.set_title(title)
-        if xlabel is not None:
-            ax.set_xlabel(xlabel)
-        elif answer is not None:
-            # The following should be cb.ax.set_xlabel, but this is broken in matplotlib 3.5.1.
-            cb.ax.set_title(f"p({str(answer).strip()})", y=-0.16, fontsize=10)
-        if savepdf:
-            os.makedirs(os.path.dirname(savepdf), exist_ok=True)
-            plt.savefig(savepdf, bbox_inches="tight")
-            plt.close()
-        else:
-            plt.show()
+    fig, ax = plt.subplots(figsize=(3.5, 2), dpi=200)
+    h = ax.pcolor(
+        differences,
+        cmap={None: "Purples", "None": "Purples", "mlp": "Greens", "attn": "Reds"}[
+            kind
+        ],
+        vmin=low_score #Setting the minimum value of the color bar to 0
+        # vmax=1    # Setting the maximum value of the color bar to 1
+    )
+    ax.invert_yaxis()
+    ax.set_yticks([0.5 + i for i in range(len(differences))])
+    ax.set_xticks([0.5 + i for i in range(0, differences.shape[1] - 6, 5)])
+    ax.set_xticklabels(list(range(0, differences.shape[1] - 6, 5)))
+    ax.set_yticklabels(labels)
+    if not modelname:
+        modelname = "GPT"
+    if not kind:
+        ax.set_title("Impact of restoring state after corrupted input")
+        ax.set_xlabel(f"single restored layer within {modelname}")
+    else:
+        kindname = "MLP" if kind == "mlp" else "Attn"
+        ax.set_title(f"Impact of restoring {kindname} after corrupted input")
+        ax.set_xlabel(f"center of interval of {window} restored {kindname} layers")
+    cb = plt.colorbar(h)
+    if title is not None:
+        ax.set_title(title)
+    if xlabel is not None:
+        ax.set_xlabel(xlabel)
+    elif answer is not None:
+        # The following should be cb.ax.set_xlabel, but this is broken in matplotlib 3.5.1.
+        cb.ax.set_title(f"p({str(answer).strip()})", y=-0.16, fontsize=10)
+    if savepdf:
+        os.makedirs(os.path.dirname(savepdf), exist_ok=True)
+        plt.savefig(savepdf, bbox_inches="tight")
+        plt.close()
+    else:
+        plt.show()
 
 
 def plot_all_flow(mt, prompt, subject=None):
