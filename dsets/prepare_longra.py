f706891f0de5956c4c51c15f65551f87ffa6244c
Reza Madani <rezamadani.ai@gmail.com>
2025-01-26 15:21:00 +0100

    Update

--- dsets/prepare_longra.py
+++ dsets/prepare_longra.py
@@ -41,38 +41,22 @@ model.to(device)
 
 with torch.no_grad():
     # Build analogies index
-
     all_analogies = preprocess_analogies(analogies, tokenizer)
-    # print(f"1111111==>> all_analogies: {all_analogies}")
     all_analogies = create_analogy_templates(all_analogies)
-    # print(' all_analogies', all_analogies.keys)
-    # print(' all_analogies', all_analogies['currency'])
-    # print(' ========= ')
-    # print(' all_analogies', all_analogies)
 
     # Build data
-
+    data = list()
     data_id = 0
     for analogy_idx, (analogy_label, analogy_config) in tqdm(enumerate(all_analogies.items())):
-        # print(analogy_idx)
-        # print(analogy_label)
-        # print(analogy_config)
         analogy_config = all_analogies[analogy_label]
 
         template = analogy_config["template"]
-        # print(f"==>> template: {template}")
 
         # map tags to target/relative
         target_tag = "a" if template.index("[A]") > template.index("[B]") else "b"
-        # print(f"==>> target_tag: {target_tag}")
         relative_tag = "a" if target_tag == "b" else "b"
-        # print(f"==>> relative_tag: {relative_tag}")
-
-        # print(f"==>> analogy_config: {analogy_config}")
 
         for pair_idx in range(len(analogy_config["a"])):
-            # print("".center(50, "-"))
-            # print(pair_idx)
             word_a = analogy_config["a"][pair_idx] # target word
             word_b = analogy_config["b"][pair_idx] # relative word
 
@@ -84,26 +68,18 @@ with torch.no_grad():
 
             distractor_removed = prompt.replace(distractor, "")
 
-
-            data = {
+            data.append({
                 "id": data_id,
                 "prompt": prompt,
                 "target": word_a,
                 "relative": word_b,
-                # "distractor": {
-                #     "start": distractor_start_pos.item(),
-                #     "end": distractor_end_pos.item(),
-                # },
-            }
-
-            # export file
-            indent = None if compact_output else 4
-            json_str = json.dumps(data, indent=indent)
-
-            filename = os.path.join(output_dir, f"{data_id}.json")
-            with open(filename, "w") as f_output:
-                f_output.write(json_str)
-                # print(filename)
-
+            })
             data_id += 1
-            logging.info(f"done {data_id}")
+
+    json_str = json.dumps(data, indent=indent)
+    filename = os.path.join(output_dir, "LongRA.json")
+    with open(filename, "w") as f_output:
+        f_output.write(json_str)
+
+
+    logging.info(f"Preparation finished")
