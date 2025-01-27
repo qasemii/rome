57fd4095dafe1231d163e3f5814157b0bc259201
Reza Madani <rezamadani.ai@gmail.com>
2025-01-27 14:57:23 +0100

    [IMPORTANT] Updated rationalization method to norm-bounded perturbation

--- experiments/rationalization.py
+++ experiments/rationalization.py
@@ -49,18 +49,8 @@ torch.set_grad_enabled(False)
 nltk.download('punkt_tab')
 
 
-def extract_rationales(
-        mt,
-        prompt,
-        samples=10,
-        noise=0.1,
-        window=1,
-        uniform_noise=False,
-        mode=None,
-        normalize=False,
-):
-    # Llama and Gemma add bos token
-    inp = make_inputs(mt.tokenizer, [prompt] * (samples + 1))  # [(samples + 1), 1]
+def get_rationales(mt, prompt, mode='prob', window=1):
+    inp = make_inputs(mt.tokenizer, [prompt] * 11)
     with torch.no_grad():
         base_scores = predict_from_input(mt.model, inp)[0]
 
@@ -68,72 +58,61 @@ def extract_rationales(
     answer_t = mt.tokenizer.encode(answer)[0]
     base_score = base_scores[answer_t]
 
-    noised_scores = make_noisy_embeddings(
-        mt, inp,
-        tokens_to_mix=[(0, len(inp))],
-        noise=noise,
-    )
-    noised_score = noised_scores[answer_t]
-
-    results = {}
-    token_scores = [0] if isinstance(mt.model, Gemma2ForCausalLM) or isinstance(mt.model, LlamaForCausalLM) else []
-
-    # Tokenize sentence into words and punctuation
     tokens = nltk.word_tokenize(prompt)
+
     tokens = ['"' if token in ['``', "''"] else token for token in tokens]
     tokens = check_whitespace(prompt, tokens)
-
     tokens_range = collect_token_range(mt, prompt, 1)
-    if window >= 1:
+    if window > 1:
         tokens_range = list(combinations(tokens_range, window))
-    score_table = torch.zeros(len(tokens_range), len(tokens))
-    for i, r_list in enumerate(tokens_range):
-        r_list = list(r_list)
-        # breakpoint()
-        try:
-            suff_scores = make_noisy_embeddings(
-                mt, inp, tokens_to_mix=r_list, noise=noise, uniform_noise=uniform_noise, denoise=True
-            )
 
-            comp_scores = make_noisy_embeddings(
-                mt, inp, tokens_to_mix=r_list, noise=noise, uniform_noise=uniform_noise
-            )
-        except:
-            print(f"Couldn't compute the low_scores. Assigning 0 to lower_score ...")
-            low_scores = torch.zeros(mt.tokenizer.vocab_size, device=mt.model.device)
+    score_table = torch.zeros(len(tokens_range), len(inp['input_ids'][0]))
+    for idx, t_range in enumerate(tokens_range):
+        t_range = [t_range] if window == 1 else list(t_range)
 
-        if mode is None:
-            suff_score = suff_scores[answer_t]
-            comp_score = comp_scores[answer_t]
-            score = (base_score - comp_score) + (suff_score - noised_score)
+        high = 10.0
+        low = 0
+        epsilon = 0.001
+
+        while (high - low > epsilon):  # Sufficient iterations for precision
+            noise = (low + high) / 2
+            with torch.no_grad():
+                low_scores = make_noisy_embeddings(mt, inp, tokens_to_mix=t_range, noise=noise)
+
+            prob = low_scores[answer_t]
+            # rank = torch.sort(low_scores, dim=-1, descending=True).indices.tolist().index(answer_t)
+            rank = (low_scores > prob).sum().item()
+            if rank == 0:
+                low = noise
+            else:
+                high = noise
+
+        print(t_range, noise, rank)
+        if mode == 'noise':
+            score = 10.0 - noise
+        elif mode == 'prob':
+            score = (base_score - prob)
         else:
-            topk = 10
-            base_scores_topk = torch.topk(base_scores, topk).values
-            topk_idx = torch.topk(base_scores, topk).indices
-            low_scores_topk = low_scores.gather(0, topk_idx)
+            raise ValueError(f'Invalid mode: {mode}')
 
-            kl_loss = torch.nn.KLDivLoss(reduction="batchmean")
-            score = kl_loss(low_scores_topk, base_scores_topk)
+        for (b, e) in t_range:
+            score_table[idx, b:e] = score
 
-        for (b, e) in r_list:
-            score_table[i, b:e] = score.item()
+    token_range = collect_token_range(mt, prompt, 1)
 
-    # breakpoint()
-    tokens_range = collect_token_range(mt, prompt, 1)
-    word_scores = torch.sum(score_table, dim=0) / torch.sum((score_table != 0), dim=0)
+    token_scores = torch.sum(score_table, dim=0) / torch.sum((score_table != 0), dim=0)
 
-    for i, r in enumerate(tokens_range):
-        n_extend = r[1] - r[0]
-        token_scores.extend([word_scores[i].item()] * n_extend)
+    word_scores = torch.tensor([token_scores[t_range[0]].item() for t_range in token_range])
+    word_scores = torch.softmax(word_scores, dim=-1)
+
+    results = {}
 
     results['input_ids'] = inp["input_ids"][0]
     results['input_tokens'] = tokens
     results['answer'] = answer
     results['base_score'] = base_score
-    results['word_scores'] = word_scores
-    results['token_scores'] = torch.tensor(token_scores, device=mt.model.device).unsqueeze(dim=0)
 
-    if normalize:
-        results['scores'] = torch.softmax(results['scores'], dim=1)
+    results['word_scores'] = word_scores.to(mt.model.device)
+    results['token_scores'] = token_scores.unsqueeze(dim=0).to(mt.model.device)
 
     return results
