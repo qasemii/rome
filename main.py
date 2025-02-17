3dd77235cc4a240bc8304a617f57309d2cf88e9b
Reza Madani <rezamadani.ai@gmail.com>
2025-02-18 00:59:42 +0100

    Update

--- main.py
+++ main.py
@@ -6,41 +6,36 @@ import nltk
 import pickle
 from tqdm import tqdm
 from pathlib import Path
-from transformers import (
-    Gemma2ForCausalLM,
-    LlamaForCausalLM,
-)
 from util.globals import DATA_DIR
-
-from experiments.utils import (
-    ModelAndTokenizer,
-    predict_token,
-)
-from experiments.rationalization import (
-    get_rationales,
-)
 
 from dsets import (
     KnownsDataset,
     CounterFactDataset,
 )
 
-from rationalization.rationalizer.aggregate_rationalizer import AggregateRationalizer
-from rationalization.rationalizer.importance_score_evaluator.delta_prob import DeltaProbImportanceScoreEvaluator
-from rationalization.rationalizer.stopping_condition_evaluator.top_k import TopKStoppingConditionEvaluator
-from rationalization.rationalizer.token_replacement.token_replacer.uniform import UniformTokenReplacer
-from rationalization.rationalizer.token_replacement.token_sampler.postag import POSTagTokenSampler
-
 from rationalization.src.evaluation.evaluator.soft_norm_sufficiency import SoftNormalizedSufficiencyEvaluator
-from rationalization.src.evaluation.evaluator.soft_norm_comprehensiveness import \
-    SoftNormalizedComprehensivenessEvaluator
+from rationalization.src.evaluation.evaluator.soft_norm_comprehensiveness import SoftNormalizedComprehensivenessEvaluator
+
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+)
 
 device = "cuda"
 
 random.seed(42)
 torch.manual_seed(42)
-# torch.use_deterministic_algorithms(True, warn_only=True)
+torch.use_deterministic_algorithms(True, warn_only=True)
 torch.set_grad_enabled(False)
+
+
+def predict_token(model, tokenizer, prompt):
+    inp = tokenizer(prompt, return_tensors='pt').to(device)
+    logits = model(**inp)["logits"]
+    probs = torch.softmax(logits[:, -1, :], dim=-1) 
+    probs, preds = torch.max(probs, dim=-1, keepdim=True)  # Keep dims for consistency
+    result = tokenizer.decode(preds.squeeze(0).item())
+    return result
 
 
 def main():
@@ -56,15 +51,11 @@ def main():
     aa("--max_new_tokens", default=1, type=int)
     aa("--norm", default='2')
     aa("--mode", default='prob')
-    aa("--method",
-       type=str,
-       default="noiser",
-       help="noiser, attention, attention_last, attention_rollout, \
-             gradient_shap, input_x_gradient, integrated_gradients, lime, reagent, occlusion")  # TODO
+    aa("--method", default="noiser", type=str,
+       help="noiser, attention, attention_last, attention_rollout, gradient_shap,\
+             input_x_gradient, integrated_gradients, lime, reagent, occlusion")  
 
     args = parser.parse_args()
-
-    nltk.download('averaged_perceptron_tagger_eng')
 
     result_dir = f"{args.output_dir}{args.dataset}/{args.model_name}"
     os.makedirs(result_dir, exist_ok=True)
@@ -73,31 +64,28 @@ def main():
     os.makedirs(cache_dir, exist_ok=True)
 
     print('Loading model and tokenizer ...')
-    mt = ModelAndTokenizer(
-        args.model_name,
-        low_cpu_mem_usage=True,
-        torch_dtype=torch.float16,
-    )
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    model = AutoModelForCausalLM.from_pretrained(args.model_name).to(device)
 
     special_tokens_dict = dict()
-    if mt.tokenizer.pad_token is None:
-        mt.tokenizer.add_special_tokens({'pad_token': '<pad>'})
-        special_tokens_dict["pad_token"] = mt.tokenizer.eos_token
-    if mt.tokenizer.bos_token is None:
-        mt.tokenizer.add_special_tokens({'bos_token': '<bos>'})
-        special_tokens_dict["bos_token"] = mt.tokenizer.bos_token
-    if mt.tokenizer.eos_token is None:
-        mt.tokenizer.add_special_tokens({'eos_token': '<eos>'})
-        special_tokens_dict["eos_token"] = mt.tokenizer.eos_token
-    if mt.tokenizer.unk_token is None:
-        mt.tokenizer.add_special_tokens({'unk_token': '<unk>'})
-        special_tokens_dict["unk_token"] = mt.tokenizer.unk_token
-    # if mt.tokenizer.bos_token is None:
-    #    mt.tokenizer.bos_token = "<bos>"
-    #    mt.tokenizer.bos_token_id = mt.tokenizer.convert_tokens_to_ids("<bos>")
+    if tokenizer.pad_token is None:
+        tokenizer.add_special_tokens({'pad_token': '<pad>'})
+        special_tokens_dict["pad_token"] = tokenizer.eos_token
+    if tokenizer.bos_token is None:
+        tokenizer.add_special_tokens({'bos_token': '<bos>'})
+        special_tokens_dict["bos_token"] = tokenizer.bos_token
+    if tokenizer.eos_token is None:
+        tokenizer.add_special_tokens({'eos_token': '<eos>'})
+        special_tokens_dict["eos_token"] = tokenizer.eos_token
+    if tokenizer.unk_token is None:
+        tokenizer.add_special_tokens({'unk_token': '<unk>'})
+        special_tokens_dict["unk_token"] = tokenizer.unk_token
+    # if tokenizer.bos_token is None:
+    #    tokenizer.bos_token = "<bos>"
+    #    tokenizer.bos_token_id = tokenizer.convert_tokens_to_ids("<bos>")
     
-    pad_token_id = mt.tokenizer.pad_token_id if mt.tokenizer.pad_token_id is not None else mt.tokenizer.eos_token_id
-
+    # pad_token_id = tokenizer.pad_token_id if tokenizer.pad_token_id is not None else tokenizer.eos_token_id
+    breakpoint()
     print(f"Loading {args.dataset} dataset ...")
     if args.dataset == "Knowns":
         dataset = KnownsDataset(DATA_DIR)
@@ -109,123 +97,125 @@ def main():
     else:
         raise ValueError
     
-    # Filter dataset to only include examples where the predicted token matches the target
-    print(f"Filtering dataset ...")
-    dataset = [
-        d for i, d in enumerate(dataset) 
-        if predict_token(mt, [d['prompt']], topk=1)[0][0].strip() == d['target']
-    ]
-    print(f"Filtered dataset to {len(dataset)} examples")
-
-    
-    # init rationalizer
-    rational_size = 3
-    rational_size_ratio = None
-
-    # tested with 3 0.1 5000 5
-    stopping_top_k = 3
-    replacing = 0.1
-    max_step = 3000
-    batch = 3
+    # # Filter dataset to only include examples where the predicted token matches the target
+    # print(f"Filtering dataset ...")
+    # dataset = [
+    #     d for d in dataset
+    #     if predict_token(model, tokenizer, d['prompt']).strip() == d['target']
+    # ]
+    # print(f"Filtered dataset to {len(dataset)} examples")
 
     if args.method == 'noiser':
         nltk.download('punkt_tab')
+        from rationalization.rationalizer.importance_score_evaluator.noiser import  NoiserImportanceScoreEvaluator
+        rationalizer = NoiserImportanceScoreEvaluator(
+            model=model,
+            tokenizer=tokenizer,
+            norm=args.norm,
+            mode=args.mode
+        )
     elif args.method == 'random':
         pass
     elif args.method == 'attention_last' or args.method == 'attention_rollout':
         from rationalization.rationalizer.importance_score_evaluator.attention import AttentionImportanceScoreEvaluator
         rationalizer = AttentionImportanceScoreEvaluator(
-            model=mt.model,
-            tokenizer=mt.tokenizer,
+            model=model,
+            tokenizer=tokenizer,
             attn_type=args.method.replace("attention_", "")
         )
-    else:
+    else: #['integrated_gradients', 'input_x_gradient', 'attention', 'gradient_shap']
         # assert args.method in ['integrated_gradients', 'input_x_gradient', 'attention', 'gradient_shap'] # input_x_gradient = signed in self written
         from rationalization.rationalizer.importance_score_evaluator.inseq import  InseqImportanceScoreEvaluator
         rationalizer = InseqImportanceScoreEvaluator(
-            model=mt.model,
-            tokenizer=mt.tokenizer,
+            model=model,
+            tokenizer=tokenizer,
             method=args.method, 
             attribute_params=special_tokens_dict
         )
 
     # init evaluator
-    soft_norm_suff_evaluator = SoftNormalizedSufficiencyEvaluator(mt.model)
-    soft_norm_comp_evaluator = SoftNormalizedComprehensivenessEvaluator(mt.model)
+    soft_norm_suff_evaluator = SoftNormalizedSufficiencyEvaluator(model)
+    soft_norm_comp_evaluator = SoftNormalizedComprehensivenessEvaluator(model)
 
     source_soft_ns = []
     source_soft_nc = []
     random_soft_ns = []
     random_soft_nc = []
-    results = []
-    print("Starting rationalization ...")
 
+    print("Starting rationalization ...")
     samples = dataset if args.n_samples == -1 else random.choices(dataset, k=args.n_samples)
     for data in tqdm(samples):
         idx = data['id']
 
-        input_ids = mt.tokenizer(data["prompt"], return_tensors='pt')['input_ids'][0].to(mt.model.device)
-        target_id = mt.tokenizer(" "+data["target"], return_tensors='pt')['input_ids'][0].to(mt.model.device)
-        # generated_texts = mt.tokenizer.decode(generated_ids)
+        input_ids = tokenizer(data["prompt"], return_tensors='pt')['input_ids'][0].to(device)
+        attention_mask = tokenizer(data["prompt"], return_tensors='pt')['attention_mask'][0].to(device)
+        generated_ids = model.generate(input_ids=torch.unsqueeze(input_ids, 0),
+                                       attention_mask= torch.unsqueeze(attention_mask, 0),
+                                       pad_token_id=tokenizer.pad_token_id,
+                                       max_new_tokens=args.max_new_tokens, 
+                                       do_sample=False)[0]
+        
+        target_id = tokenizer(" "+data["target"], return_tensors='pt')['input_ids'][0].to(device)
+        # generated_texts = tokenizer.decode(generated_ids)
         # print(f'generated full sequence --> {generated_texts}')
 
-        # Gemma and Llama add [bos] token which should be exclude from input prompt when
-        if args.method == 'noiser':
-            ers = get_rationales(mt,
-                                 data["prompt"],
-                                 norm=args.norm,
-                                 mode=args.mode
-                                 )
-            scores = ers['token_scores']
-        elif args.method == 'random':
-            scores = torch.softmax(
-                torch.rand(torch.unsqueeze(input_ids, 0).shape, device=mt.model.device), dim=-1)
-        else:
-            rationalizer.rationalize(torch.unsqueeze(input_ids, 0), torch.unsqueeze(target_id, 0))
-            scores = rationalizer.mean_important_score.unsqueeze(dim=0).to(mt.model.device)
-            if args.method=='occlusion':
-                scores = scores/torch.sum(scores)
-        # importance score by Random Score
-        rand_scores = torch.softmax(
-            torch.rand(torch.unsqueeze(input_ids, 0).shape, device=mt.model.device), dim=-1)
+        s_soft_ns = []
+        s_soft_nc = []
+        r_soft_ns = []
+        r_soft_nc = []
 
-        try:
+        score_map = torch.zeros([generated_ids.shape[0] - input_ids.shape[0], generated_ids.shape[0] - 1], device=device)
+        for target_pos in torch.arange(input_ids.shape[0], generated_ids.shape[0]):
+            input_ids = torch.unsqueeze(generated_ids[:target_pos], 0)
+            target_id = torch.unsqueeze(generated_ids[target_pos], 0)
+
+            # rationalization
+            rationalizer.rationalize(input_ids, target_id)
+            scores = rationalizer.mean_important_score.unsqueeze(0).to(device)
+            # if args.method=='occlusion':
+            #     scores = scores/torch.sum(scores)
+
+            random_score = torch.softmax(torch.rand(scores.shape, device=device), dim=-1)
+
             # compute Soft-NS and Soft-NC on source importance score
-            source_soft_ns_step = soft_norm_suff_evaluator.evaluate(torch.unsqueeze(input_ids, 0),
-                                                                    torch.unsqueeze(target_id, 0), scores)
-            source_soft_nc_step = soft_norm_comp_evaluator.evaluate(torch.unsqueeze(input_ids, 0),
-                                                                    torch.unsqueeze(target_id, 0), scores)
-            # print(f"Source Soft-NS: {source_soft_ns_step}, Source Soft-NC: {source_soft_nc_step}")
+            s_soft_ns_step = soft_norm_suff_evaluator.evaluate(input_ids, target_id, scores)
+            s_soft_ns.append(s_soft_ns_step)
+
+            s_soft_nc_step = soft_norm_comp_evaluator.evaluate(input_ids, target_id, scores)
+            s_soft_nc.append(s_soft_nc_step)
 
             # compute Soft-NS and Soft-NC on random importance score
-            random_soft_ns_step = soft_norm_suff_evaluator.evaluate(torch.unsqueeze(input_ids, 0),
-                                                                    torch.unsqueeze(target_id, 0), rand_scores)
-            random_soft_nc_step = soft_norm_comp_evaluator.evaluate(torch.unsqueeze(input_ids, 0),
-                                                                    torch.unsqueeze(target_id, 0), rand_scores)
-            # print(f"Random Soft-NS: {random_soft_ns_step}, Random Soft-NC: {random_soft_nc_step}")
+            r_soft_ns_step = soft_norm_suff_evaluator.evaluate(input_ids, target_id, random_score)
+            r_soft_ns.append(r_soft_ns_step)
 
-            source_soft_ns.append(source_soft_ns_step.item())
-            source_soft_nc.append(source_soft_nc_step.item())
+            r_soft_nc_step = soft_norm_comp_evaluator.evaluate(input_ids, target_id, random_score)
+            r_soft_nc.append(r_soft_nc_step)
 
-            random_soft_ns.append(random_soft_ns_step.item())
-            random_soft_nc.append(random_soft_nc_step.item())
+        # # compute metrics on Soft-NS and Soft-NC
+        # soft_ns = torch.log(torch.sum(torch.tensor(source_soft_ns)) / torch.sum(torch.tensor(random_soft_ns)))
+        # n_soft_ns.append(soft_ns.item())
 
-        except:
-            print(f"Unable to get the score for {idx}")
-            continue
+        # soft_nc = torch.log(torch.sum(torch.tensor(source_soft_nc)) / torch.sum(torch.tensor(random_soft_nc)))
+        # n_soft_nc.append(soft_nc.item())
 
-        results.append({'id': data["id"],
-                        'prompt': data["prompt"],
-                        'target': data["target"],
-                        'attributions': scores})
+        source_soft_ns.append(torch.mean(torch.tensor(s_soft_ns)).item())
+        source_soft_nc.append(torch.mean(torch.tensor(s_soft_nc)).item())
 
-            
-    # # compute metrics on Soft-NS and Soft-NC
-    metric_soft_ns = torch.log(torch.mean(torch.tensor(source_soft_ns)) / torch.mean(torch.tensor(random_soft_ns)))
-    metric_soft_nc = torch.log(torch.mean(torch.tensor(source_soft_nc)) / torch.mean(torch.tensor(random_soft_nc)))
-    print(f"metric_soft_ns: {metric_soft_ns}, metric_soft_nc: {metric_soft_nc}")
+        random_soft_ns.append(torch.mean(torch.tensor(r_soft_ns)).item())
+        random_soft_nc.append(torch.mean(torch.tensor(r_soft_nc)).item())
+
+    source_soft_ns = torch.tensor(source_soft_ns)
+    source_soft_nc = torch.tensor(source_soft_nc)
+
+    random_soft_ns = torch.tensor(random_soft_ns)
+    random_soft_nc = torch.tensor(random_soft_nc)
+
+    soft_ns = torch.log(torch.mean(torch.tensor(source_soft_ns)) / torch.mean(torch.tensor(random_soft_ns)))
+    soft_nc = torch.log(torch.mean(torch.tensor(source_soft_nc)) / torch.mean(torch.tensor(random_soft_nc)))
+
+    print(f"Normalized Soft-NS: {soft_ns}")
+    print(f"Normalized Soft-NC: {soft_nc}")
     
-
     # export results
     # Path(result_dir).mkdir(exist_ok=True, parents=True)
     # method = f'{args.method}_{args.norm}_{args.mode}' if args.method == 'noiser' else args.method
